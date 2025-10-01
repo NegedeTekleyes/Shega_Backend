6429181efd8b38d7dc0c5a9140ff1bd60edb2a91
Negede <negedetekleyes@gmail.com>
2025-10-02 00:39:04 +0300

    Update signup dto

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,15 +1,18 @@
-import { IsEmail, IsEnum, IsOptional, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 import { Role } from '@prisma/client';
 
 export class SignupDto {
   @IsEmail()
+   @IsNotEmpty()
   email: string;
 
   @IsString()
-  @MinLength(6)
+  @IsNotEmpty()
+  @MinLength(8)
   password: string;
 
   @IsEnum(Role)
+  @IsOptional()
   role: Role;
 
   @IsString()
