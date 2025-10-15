356ab2855b68279da1390ebb16e6f6185d61450b
Negede <negedetekleyes@gmail.com>
2025-10-16 00:02:06 +0300

    Update create technical

--- src/technician/dto/create-technician.dto.ts
+++ src/technician/dto/create-technician.dto.ts
@@ -1,14 +1,26 @@
-import { IsInt, IsString, IsNotEmpty, IsOptional, IsEnum } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional, IsEnum, IsEmail } from 'class-validator';
 import { Speciality, TechnicianStatus } from '@prisma/client';
 
 export class CreateTechnicianDto {
-  @IsInt()
+  @IsString()
   @IsNotEmpty()
-  userId: number;
+  name: string;
 
-    @IsEnum(Speciality) 
-    @IsOptional()
-  speciality: Speciality;
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsEnum(Speciality) 
+  @IsOptional()
+  speciality?: Speciality;
 
   @IsEnum(TechnicianStatus)
   @IsOptional()
