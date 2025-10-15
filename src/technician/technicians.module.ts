a67793332acf3669253252acc5e8f69017376285
Negede <negedetekleyes@gmail.com>
2025-10-16 00:01:22 +0300

    Update module

---
+++ src/technician/technicians.module.ts
@@ -0,0 +1,13 @@
+// // src/technicians/technicians.module.ts
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../prisma/prisma.module';
+import { TechniciansController } from './technicians.controller';
+import { TechniciansService } from './technicians.service';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [TechniciansController],
+  providers: [TechniciansService],
+  exports: [TechniciansService],
+})
+export class TechniciansModule {}
