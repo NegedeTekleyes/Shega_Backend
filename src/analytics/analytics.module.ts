f4b52b1d458cbafc11ba71e8bcdd67ec50421ce3
Negede <negedetekleyes@gmail.com>
2025-10-16 00:11:58 +0300

    Update use guards

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { AnalyticsController } from "./analytics.controller";
 import { AnalyticsService } from "./analytics.service";
+import { AuthModule } from "src/auth/auth.module";
 
 
 @Module({
-    imports: [PrismaModule],
+    imports: [PrismaModule, AuthModule],
     controllers: [AnalyticsController],
     providers: [AnalyticsService],
     exports: [AnalyticsService],
