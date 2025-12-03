2685e7c43969982932bd0610a6dd527bc1c8b7d3
Negede <negedetekleyes@gmail.com>
2025-12-03 10:23:52 +0300

    final update and code review

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { PrismaModule } from '../prisma/prisma.module';
-// import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [PrismaModule],
